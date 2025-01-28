59c7572253eb4bf557f9906703e174e6a5b81322
G7gT5qzsbLRDLbm <if.dev327@gmail.com>
2025-01-27 21:21:29 -0300

    -

--- src/components/SingleApp.jsx
+++ src/components/SingleApp.jsx
@@ -11,61 +11,55 @@ const SingleApp = () => {
   let { name } = useParams()
   let projectSelected = projects.find((project) => project.name === name)
   return (
-    <section className="h-full mb-14 max-w-xl w-full ">
+    <section className="h-full mb-14 max-w-xl">
       <Fade direction="up" triggerOnce={true}>
         <Link to="/" className="flex items-center text-[15px] font-medium py-2 return">
           <BsArrowLeftShort className="bg-slate-100 rounded-full text-slate-950 me-2 arrow" />
           Back to Home
         </Link>
       </Fade>
-      <div className='flex flex-col md:flex-row justify-between items-start w-full gap-8 px-4'>
-        <div className='div1 w-[50vw]'>
-          <div className="flex flex-wrap break-words">
-            <h2 className="text-6xl font-bold mb-9 max-sm:text-[40px]">{projectSelected.title}</h2>
-          </div>
-          <p className="text-[#888b8f]">{projectSelected.resume}</p>
-
-          {/* Links to repo and live */}
-          <div className="flex justify-around mt-4 mb-10 max-sm:flex-col max-sm:items-center max-sm:py-6">
-            <a
-              target="blank"
-              href={projectSelected.url1}
-              className="flex justify-center items-center bg-[#343639] mr-3 w-[14rem] my-7 py-3 px-6 rounded-full font-semibold text-sm hover:scale-105 hover:transition-all hover:ease-in-out max-sm:my-3 max-sm:w-[14rem]"
-            >
-              <FiGithub style={{ fontSize: '1.3rem' }} />
-              <p className="ml-3">View source code</p>
-            </a>
-
-            <a
-              target="blank"
-              href={projectSelected.url2}
-              className="flex justify-center items-center bg-[#343639] mr-3 w-[14rem] my-7 py-3 px-6 rounded-full font-semibold text-sm hover:scale-105 hover:transition-all hover:ease-in-out max-sm:my-0 max-sm:w-[14rem]"
-            >
-              <RiExternalLinkFill style={{ fontSize: '1.3rem' }} />
-              <p className="ml-3">View live project</p>
-            </a>
-          </div>
-
-          <img
-            src={projectSelected.image1}
-            alt={projectSelected.title}
-            title={projectSelected.title}
-            className="rounded-2xl w-full object-cover max-sm:h-52"
-          />
-        </div>
-
-        <div className='div2 flex flex-col w-full'>   
-          <img
-            src={projectSelected.image2}
-            alt={projectSelected.title}
-            title={projectSelected.title}
-            className="rounded-2xl w-[100vw] object-cover max-sm:h-52 max-h-[378px]"
-          />
-          <p className="text-[#888b8f] my-14">{projectSelected.info1}</p>
-          <p className="text-[#888b8f] mt-14">{projectSelected.info2}</p>
-          
-        </div>
+      <div className="flex flex-wrap w-[100%] break-words">
+        <h2 className="text-6xl font-bold mb-9 max-sm:text-[40px]">{projectSelected.title}</h2>
       </div>
+      <p className="text-[#888b8f]">{projectSelected.resume}</p>
+
+      {/* Links to repo and live */}
+      <div className="flex justify-around mt-4 mb-10 max-sm:flex-col max-sm:items-center max-sm:py-6">
+        <a
+          target="blank"
+          href={projectSelected.url1}
+          className="flex justify-center items-center bg-[#343639] mr-3 w-[14rem] my-7 py-3 px-6 rounded-full font-semibold text-sm hover:scale-105 hover:transition-all hover:ease-in-out max-sm:my-3 max-sm:w-[14rem]"
+        >
+          <FiGithub style={{ fontSize: '1.3rem' }} />
+          <p className="ml-3">View source code</p>
+        </a>
+
+        <a
+          target="blank"
+          href={projectSelected.url2}
+          className="flex justify-center items-center bg-[#343639] mr-3 w-[14rem] my-7 py-3 px-6 rounded-full font-semibold text-sm hover:scale-105 hover:transition-all hover:ease-in-out max-sm:my-0 max-sm:w-[14rem]"
+        >
+          <RiExternalLinkFill style={{ fontSize: '1.3rem' }} />
+          <p className="ml-3">View live project</p>
+        </a>
+      </div>
+
+      <img
+        src={projectSelected.image1}
+        alt={projectSelected.title}
+        title={projectSelected.title}
+        className="rounded-2xl w-full object-cover max-sm:h-52"
+      />
+
+      <img
+        src={projectSelected.image2}
+        alt={projectSelected.title}
+        title={projectSelected.title}
+        className="rounded-2xl w-[100vw] object-cover max-sm:h-52 max-h-[378px]"
+      />
+      <p className="text-[#888b8f] my-14">{projectSelected.info1}</p>
+      <p className="text-[#888b8f] mt-14">{projectSelected.info2}</p>
+
       <Footer />
     </section>
   )
